e4346b6250ca908dd38e2ec45f1ef5eca88552d2
JadenMathias <mathias.jaden@gmail.com>
2022-07-06 19:38:12 -0400

    about page changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import * as React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./pages/home";
+import About from "./pages/about";
 
 const App = () => {
 	return (
 		<Layout>
 			<Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<div></div>} />
+				<Route path="/about" element={<About />} />
 				<Route path="/projects" element={<div></div>} />
 			</Routes>
 		</Layout>
--- src/components/typer.js
+++ src/components/typer.js
@@ -11,16 +11,16 @@ const Typer = (props) => {
 
 	// typeWriter
 	React.useEffect(() => {
-		if (index == words.length - 1 && subIndex == words[index].length) {
+		if (index === words.length - 1 && subIndex === words[index].length) {
 			return;
 		}
 
-		if (subIndex == words[index].length + 1 && index != words.length - 1 && !reverse) {
+		if (subIndex === words[index].length + 1 && index !== words.length - 1 && !reverse) {
 			setReverse(true);
 			return;
 		}
 
-		if (subIndex == 0 && reverse) {
+		if (subIndex === 0 && reverse) {
 			setReverse(false);
 			setIndex((prev) => prev + 1);
 			return;
@@ -28,7 +28,7 @@ const Typer = (props) => {
 
 		const timeout = setTimeout(() => {
 			setSubIndex((prev) => prev + (reverse ? -1 : 1));
-		}, Math.max(reverse ? 75 : subIndex == words[index].length ? 100 : 50, parseInt(Math.random() * 200)));
+		}, Math.max(reverse ? 75 : subIndex === words[index].length ? 100 : 50, parseInt(Math.random() * 200)));
 
 		return () => clearTimeout(timeout);
 	}, [subIndex, index, reverse]);
